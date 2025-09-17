3911d56ea1b906246ad2c77508d3e8aa08cc3402
Sublime <david.ramire@anahuac.mx>
2025-09-17 11:09:06 -0700

    Forzar actualización CSS y HTML

--- script.js
+++ script.js
@@ -47,6 +47,12 @@ const textosTabs = {
   "tab-protagonista": { en: "Protagonist", es: "Protagonista" }
 };
 
+// Dentro de updateLanguage()
+document.querySelectorAll('.tab-btn').forEach(btn => {
+  const target = btn.getAttribute('data-target');
+  btn.textContent = isEnglish ? textosTabs[target].en : textosTabs[target].es;
+});
+
 // ========================
 // Función para actualizar el contenido según el idioma
 function updateLanguage() {
