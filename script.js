0e485ea35baafe5c9c07b9ef9b6e133b90f16ce1
Sublime <david.ramire@anahuac.mx>
2025-09-17 09:16:58 -0700

    Forzar actualización CSS y HTML

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const datos = {
   nombre: "6x9: A Virtual Experience of Solitary Confinement",
   anio: "2016",
-  medioInfo: {
-    en: "The Guardian is a British newspaper founded in 1821, recognized worldwide for its independent journalism, investigative reporting, and progressive editorial stance.",
-    es: "The Guardian es un periódico británico fundado en 1821, reconocido mundialmente por su periodismo independiente, reportajes de investigación y postura editorial progresista."
+  medio: {
+    en: "The Guardian\nBritish newspaper known for independent journalism",
+    es: "The Guardian\nPeriódico británico reconocido por su periodismo independiente"
   },
   formato: "Realidad Virtual / 360°",
   url: "https://www.theguardian.com/world/ng-interactive/2016/apr/27/6x9-a-virtual-experience-of-solitary-confinement#gvr-360",
